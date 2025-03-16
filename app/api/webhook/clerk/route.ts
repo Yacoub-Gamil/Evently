914e30456d929950ffe365e03f883353d4160020
Yacoub-gamil <yacoub-gamil@hotmail.com>
2025-03-16 16:09:20 +0200

    update actions

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -57,22 +57,25 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
-  }
-  const user = {
-    clerkId: id,
-    email: email_addresses[0].email_addresses,
-    username: username,
-    firstname: first_name,
-    lastname: last_name,
-    photo: image_url,
-  };
-  const newUser = await createUser(user);
-  if (newUser) {
-    await clerkClient.users.updateUserMetadata(id, {
-      publicMetadata: {
-        userId: newUser._id,
-      },
-    });
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
+
+    const newUser = await createUser(user);
+
+    if (newUser) {
+      (await clerkClient()).users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
     return NextResponse.json({ message: "ok", user: newUser });
   }
   if (eventType === "user.updated") {
